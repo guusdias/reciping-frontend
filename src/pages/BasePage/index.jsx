9ce1ff0cc69929ea90a033ab9777071d07da2b97
Lucaswillians <lucaswilianserpa@gmail.com>
2024-04-09 22:12:19 -0300

    feat:: header and routes

--- src/pages/BasePage/index.jsx
+++ src/pages/BasePage/index.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/SidebarContainer";
 import { Container } from "postcss";
 import RecipesFeed from "../../components/RecipiesFeed";
+import Header from "../../components/Header";
 
 export default function BasePage() {
   return (
     <main>
       <div className="flex justify-around">
+        <Header/>
         <Sidebar />
         <Outlet />
       </div>
--- src/utils/Data.jsx
+++ src/utils/Data.jsx
@@ -2,30 +2,35 @@ import { CgProfile } from "react-icons/cg";
 import { GrFavorite } from "react-icons/gr";
 import { FaList } from "react-icons/fa6";
 import { RiLogoutCircleLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 export const datas = [
   
   {
     id: 1,
     icon: <CgProfile />,
-    text: "Profile",
+    text: 'My profile',
+    path: '/profile'
   },
 
   {
     id: 2,
     icon: <GrFavorite />,
     text: "Favorites",
+    path: '/favorites'
   },
 
   {
     id: 3,
     icon: <FaList />,
     text: "My Recipes",
+    path: '/recipes'
   },
   
   {
     id: 4,
     icon: <RiLogoutCircleLine />,
     text: "Logout",
+    path: '/login'
   }
 ]
