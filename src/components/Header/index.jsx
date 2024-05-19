636114d2d66e8a6d0a162bc0b2ed5d440ca31ff2
gustapine <gustavohd34@gmail.com>
2024-05-19 16:49:36 -0300

    fix: search style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <input
           type="search"
           placeholder="Search Recipes"
-          className="rounded-md px-4 py-2 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          className="rounded-md px-4 py-2 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 text-black"
         />
         <button className="ml-4 bg-indigo-700 text-white py-2 px-4 rounded-md hover:bg-indigo-800">
           Search
