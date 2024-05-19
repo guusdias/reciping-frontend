3b8757781d7ab8cb2ead5ca2a6afc882b6f54a05
gustapine <gustavohd34@gmail.com>
2024-05-19 15:36:20 -0300

    fix: format styles

--- src/pages/BasePage/index.jsx
+++ src/pages/BasePage/index.jsx
@@ -1,18 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/SidebarContainer";
-import RecipesFeed from "../../components/RecipiesFeed";
 import Header from "../../components/Header";
 import FavoritesProvider from "../../contexts/Favorite";
 
 export default function BasePage() {
   return (
-    <main>
-      <div className="flex justify-around">
-        <Header/>
+    <main className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-1 gap-4">
+        <aside className="bg-gray-200 w-1/4 p-4 flex justify-center items-center">
           <Sidebar />
-            <FavoritesProvider>
-             <Outlet />   
+        </aside>
+        <section className="flex-1 p-4">
+          {" "}
+          {/* Adiciona padding para dar espaçamento */}
+          <FavoritesProvider>
+            <Outlet />
           </FavoritesProvider>
+        </section>
+        <aside className="bg-gray-200 w-1/4 p-4">
+          {" "}
+          {/* Ajusta o segundo aside */}
+          {/* Conteúdo do segundo aside */}
+        </aside>
       </div>
     </main>
   );
