01881c122f2ed67e3eab2e1717470644e29356cf
gustapine <gustavohd34@gmail.com>
2024-05-19 13:37:04 -0300

    init add recipie arch

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Profile from "./pages/Profile/index.jsx";
 import BasePage from "./pages/BasePage/index.jsx";
 import Feed from "./pages/Feed/index.jsx";
-import Addrecipe from "./components/Addrecipe/index.jsx"
-import NotFound from "./pages/NotFound/index.jsx"
+import Addrecipe from "./components/AddRecipe/index.jsx";
+import NotFound from "./pages/NotFound/index.jsx";
 import Favorites from "./pages/Favorites/index.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    errorElement: <NotFound/>,
+    errorElement: <NotFound />,
     element: <BasePage />,
     children: [
       { path: "/feed", exact: true, element: <Feed /> },
       { path: "/profile", element: <Profile /> },
-      { path: "/addRecipe", exact: true, element: <Addrecipe/>  },
-      { path: "/favorites", exact: true, element: <Favorites/>  },
+      { path: "/addRecipe", exact: true, element: <Addrecipe /> },
+      { path: "/favorites", exact: true, element: <Favorites /> },
     ],
   },
 ]);
