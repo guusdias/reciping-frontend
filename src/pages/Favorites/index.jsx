636ce0223f58f5eb325189cd0418ea53e22f008e
guusdias <gustavohd34@gmail.com>
2024-06-22 09:23:00 -0300

    fix: fav page styles

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,18 +1,18 @@
 import Recipe from "../../components/Recipe";
-import { useFavoriteContext } from "../../contexts/Favorite"
+import { useFavoriteContext } from "../../contexts/Favorite";
 
-export default function Favorites () {
+export default function Favorites() {
   const { favorite } = useFavoriteContext();
 
   return (
-    <div className="favorites-recipies">
-      <section className="space-y-4">
-        {
-          favorite.map((fav) => {
-            return <Recipe {...fav} key={fav.id} />
-          })
-        }
-      </section>
+    <div className="flex flex-col mt-0 gap-10 items-left shadow-md p-10 rounded-3xl bg-slate-50 h-full w-full">
+      <div className="favorites-recipies ">
+        <section className="space-y-4">
+          {favorite.map((fav) => {
+            return <Recipe {...fav} key={fav.id} />;
+          })}
+        </section>
+      </div>
     </div>
-  )
+  );
 }
