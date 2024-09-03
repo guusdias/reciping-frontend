8223892488ffd71db4b438da4bf0ba58c35df81f
guusdias <gustavohd34@gmail.com>
2024-09-03 17:31:50 -0300

    fix: not found

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>404 - Página não encontrada</h1>
+      <p>A página que você está procurando não existe ou foi movida.</p>
+      <Link to="/feed">Voltar para a página inicial</Link>
+    </div>
+  );
+};
+
+export default NotFound;
